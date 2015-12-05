31218c3919010ec93b0c6a8bd8bcb2aaf065d7e1
zhangxiuyun <zhangxiuyun@shinezone.com>
2015-12-05 15:36:54 +0800

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set( 'display_errors', 'On' );
+
+define('APPLICATION_PATH', dirname(__FILE__));
+
+$application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
+
+$application->bootstrap()->run();
+?>
